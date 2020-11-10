4f6a8e4617c0033032b443dcb7aad497b4ac3f50
Sara Strong <sara.n.strong@gmail.com>
2020-11-10 17:01:21 -0500

    Testing for user model attributes, signup, authenticate

--- test_user_model.py
+++ test_user_model.py
@@ -8,7 +8,7 @@
 import os
 from unittest import TestCase
 
-from models import db, User, Message, Follows
+from models import db, bcrypt, User, Message, Follows
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -16,7 +16,6 @@ from models import db, User, Message, Follows
 # connected to the database
 
 os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
-
 
 # Now we can import app
 
@@ -26,6 +25,7 @@ from app import app
 # once for all tests --- in each test, we'll delete the data
 # and create fresh new clean test data
 
+db.drop_all()
 db.create_all()
 
 
@@ -41,13 +41,18 @@ class UserModelTestCase(TestCase):
 
         self.client = app.test_client()
 
+    def tearDown(self):
+        """Clean up any fouled transactions."""
+
+        db.session.rollback()
+
     def test_user_model(self):
         """Does basic model work?"""
 
         u = User(
             email="test@test.com",
             username="testuser",
-            password="HASHED_PASSWORD"
+            password="UNHASHED_PASSWORD"
         )
 
         db.session.add(u)
@@ -56,3 +61,51 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+
+        # Check User attributes
+        self.assertEqual(u.email, 'test@test.com')
+        self.assertEqual(u.username, 'testuser')
+        self.assertEqual(u.password, 'UNHASHED_PASSWORD') # at this point password has not been hashed
+        self.assertEqual(u.image_url, "/static/images/default-pic.png")
+        self.assertEqual(u.header_image_url, "/static/images/warbler-hero.jpg")
+
+    def test_signup(self):
+        """Tests signup process"""
+
+        u = User.signup(
+            email="test@test.com",
+            username="testuser",
+            password="HASHED_PASSWORD",
+            image_url=None
+        )
+        db.session.commit()
+
+        self.assertEqual(u.username, "testuser")
+        self.assertEqual(u.image_url, "/static/images/default-pic.png")
+        self.assertNotEqual(u.password, "HASHED_PASSWORD")
+
+    def test_authenticate(self):
+        """Tests authentication"""
+        u = User.signup(
+            email="test@test.com",
+            username="testuser",
+            password="HASHED_PASSWORD",
+            image_url=None
+        )
+        db.session.commit()
+
+        authenticated1 = User.authenticate(username="testuser", password="hash_it_up")
+        self.assertEqual(authenticated1, False)
+
+        authenticated2 = User.authenticate(username="testuser", password="HASHED_PASSWORD")
+        self.assertNotEqual(authenticated2.password, "HASHED_PASSWORD")
+        self.assertEqual(authenticated2.password, u.password)
+
+    
+
+
+
+
+
+
+
